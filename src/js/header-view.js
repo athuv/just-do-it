d57aabb1abdcf0b8159f9cf6559a8ab4646451e2
Ahamed Athhar <21102277+athuv@users.noreply.github.com>
2023-06-18 07:21:58 +0530

    Refactor: Removed unused styles and modularized header-view.js
    - Removed unused styles from style.css
    - Modularized header-view.js by breaking it down into smaller, more manageable functions.

--- src/js/header-view.js
+++ src/js/header-view.js
@@ -1,28 +1,33 @@
 import { homeIcon, hamburgerMenuIcon } from './icons';
 
-export default function headerView() {
-  const headerElement = document.querySelector('header');
-  const sections = ['header-left-section', 'header-right-section'];
-
-  sections.forEach((section) => {
+function createHeaderSection(headerElement, sectionNames) {
+  sectionNames.forEach((section) => {
     const sectionElement = document.createElement('section');
     sectionElement.classList.add(section);
     headerElement.appendChild(sectionElement);
   });
+}
 
-  const leftSection = headerElement.querySelector(':first-child');
-
+function createHamburgerMenu(leftSection) {
   const divHamburgerMenuBtn = document.createElement('button');
   divHamburgerMenuBtn.classList.add('header-left-section__hamburger-menu');
   divHamburgerMenuBtn.innerHTML = hamburgerMenuIcon();
   leftSection.appendChild(divHamburgerMenuBtn);
+}
 
+function createHomeButton(leftSection) {
   const divHomeBtn = document.createElement('button');
   divHomeBtn.classList.add('header-left-section__home-button');
   divHomeBtn.innerHTML = homeIcon();
   leftSection.appendChild(divHomeBtn);
-  
-  const main = document.querySelector('main');
-  const as = main.querySelector(':first-child');
-  as.textContent = 'Projects';
 }
+
+export default function headerView() {
+  const headerElement = document.querySelector('header');
+  createHeaderSection(headerElement, ['header-left-section', 'header-right-section']);
+
+  const leftSection = headerElement.querySelector(':first-child');
+  createHamburgerMenu(leftSection);
+  createHomeButton(leftSection);
+}
+
